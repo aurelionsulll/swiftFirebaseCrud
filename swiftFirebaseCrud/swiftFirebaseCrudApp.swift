8a0f3362bf42b63c11beb62a45ef904b6b2a4ef6
aurelionsulll <mennanamehdi@gmail.com>
2021-10-08 15:21:42 +0100

    Initial Commit

---
+++ swiftFirebaseCrud/swiftFirebaseCrudApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftFirebaseCrudApp.swift
+//  swiftFirebaseCrud
+//
+//  Created by mehdi on 08/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct swiftFirebaseCrudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
